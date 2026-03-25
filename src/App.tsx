c4a6c055d2c0eda7fd78e8411abdf92b44a16584
Andy <cptnandres@outlook.com>
2026-03-24 18:35:28 -0600

    chore: initial Vite + React + TS setup

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+const containerStyle = {
+  display: 'flex',
+  height: '100vh',
+  alignItems: 'center',
+  justifyContent: 'center',
+};
+
+function App() {
+  return (
+    <div style={containerStyle}>
+      <div>React Query Perf Lab 🚀</div>
+    </div>
+  );
+}
+
+export default App;
